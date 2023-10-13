d9852cb33f1c7567de560c8456e2b19418b18f5c
sophie <sophie.boubet@eleve.ensai.fr>
2023-10-13 16:59:59 +0200

    corpus des classes 2

---
+++ src/business_object/listeenvie.py
@@ -0,0 +1,13 @@
+class ListeEnvie :
+
+    def __init__(
+        self,
+        id_list,
+        envies
+    ):
+        self.id_list = id_list
+        self.envies = envies
+    
+    def ajouterEnvie(self):
+
+    def retirerEnvie(self):
