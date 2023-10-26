66892946c80e4a6dc2a4a709aa5d4b88b1c7be8d
sophie <sophie.boubet@eleve.ensai.fr>
2023-10-26 16:36:37 +0200

    methodes de la classe professeur faites à partir de celles de la dao de prof

--- src/client/professeur.py
+++ src/client/professeur.py
@@ -1,13 +1,17 @@
 from utilisateur import Utilisateur
+from dao.professeur_dao import *
 
 class Professeur(Utilisateur):
     def __init__(self):
         super().__init__()
 
     def add_stage(self, stage_data):
-        if self.authenticated:
-            # Ajout de stage
-            # ...
+        """ 
+        Méthode qui permet à un(e) professeur(e) d'ajouter un stage
+        
+        retourne : un str qui indique si la tâche a été effectuée
+        """ 
+        return 
 
     def remove_stage(self, stage_id):
         if self.authenticated:
