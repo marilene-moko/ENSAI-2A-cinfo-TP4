0db1322a02a39bbef4c5f7ce64a17d191cd0de1f
sophie <sophie.boubet@eleve.ensai.fr>
2023-10-26 12:12:23 +0200

    amélioration dao prof pour eviter injection sql et ajout de la fonction modifierdroitsutilisateurs à dao admin

--- src/dao/professeur_dao.py
+++ src/dao/professeur_dao.py
@@ -6,53 +6,63 @@ from dao.utilisateur_dao import UtilisateurDao
 
 
 class ProfesseurDao(UtilisateurDao):
-    def ajouterStage(
-        self,
-        utilisateur,
-        titre_stage,
-        URL,
-        categorie_stage,
-        ville_stage,
-        poste_stage,
-    ):
-        email = utilisateur["email"]
+    def ajouterStage(self, email, titre_stage, URL, categorie_stage, ville_stage, poste_stage):
+
+    # Vérifier que le stage n'existe pas
+    with DBConnection().connection as connection:
+        with connection.cursor() as cursor:
+            cursor.execute(
+                'SELECT * '
+                'FROM "Projet_Info".Stage '
+                'WHERE URL_stage = %s;',
+                (URL,)
+            )
+            res = cursor.fetchone()
+
+    if not res:
+        with DBConnection().connection as connection:
+            with connection.cursor() as cursor:
+                cursor.execute(
+                    'INSERT INTO "Projet_Info".Stage (URL_stage, Categorie, Intitule, Ville, Poste, identifiant_personne_ajout) '
+                    'VALUES (%s, %s, %s, %s, %s, %s);',
+                    (URL, categorie_stage, titre_stage, ville_stage, poste_stage, email)
+                )
+                
+                if cursor.rowcount > 0:
+                    return "Le stage a été ajouté avec succès !"
+                else:
+                    return "Un problème est survenu et le stage n'a pas pu être ajouté."
+    return "Le stage existe déjà."
 
 
-        #if => vérifier si le stage n'est pas déjà dans la BDD
-            with DBConnection().connection as connection:
-                with connection.cursor() as cursor:
-                    cursor.execute(
-                        'INSERT INTO "Projet_Info".stage(URL_stage, Categorie, Intitule, Ville, Poste, identifiant_personne_ajout) VALUE '
-                        '(URL, categorie_stage, titre_stage, ville_stage, poste_stage, email);                                     '
-                    )
-                    
-                    if cursor.rowcount > 0:
-                        return "Le stage a été ajouté avec succès !"
-                    else:
-                        return "Un problème est survenu et le stage n'a pas pu être ajouté"
-    
 
+    def retirerStage(self, email, URL):
 
-    def retirerStage(
-        self,
-        utilisateur,
-        URL,
-    ):
-        email = utilisateur["email"]
+    # Vérifier que le stage existe
+    with DBConnection().connection as connection:
+        with connection.cursor() as cursor:
+            cursor.execute(
+                'SELECT * '
+                'FROM "Projet_Info".Stage '
+                'WHERE URL_stage = %s;',
+                (URL,)
+            )
+            res = cursor.fetchone()
 
-
-        #if => vérifier si le stage est bien dans la BDD
-            with DBConnection().connection as connection:
-                with connection.cursor() as cursor:
-                    cursor.execute(
-                        'DELETE "Projet_Info".stage '
-                        'WHERE URL_stage = URL;     '
-                    )
-                    
-                    if cursor.rowcount > 0:
-                        return "Le stage a été retiré avec succès !"
-                    else:
-                        return "Un problème est survenu et le stage n'a pas pu être retiré"
+    if res:
+        with DBConnection().connection as connection:
+            with connection.cursor() as cursor:
+                cursor.execute(
+                    'DELETE FROM "Projet_Info".stage '
+                    'WHERE URL_stage = %s;',
+                    (URL,)
+                )
+                
+                if cursor.rowcount > 0:
+                    return "Le stage a été retiré avec succès !"
+                else:
+                    return "Un problème est survenu et le stage n'a pas pu être retiré."
+    return "Ce stage n'existe pas."
 
 
 
