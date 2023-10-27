c14aed13928a8861d9b554541f5da36b2a6c5501
Clément <corbibac@gmail.com>
2023-10-27 14:30:57 +0200

    ajout self

--- src/services/historique_service.py
+++ src/services/historique_service.py
@@ -7,32 +7,32 @@ class HistoriqueService:
         Call the DAO function to retrieve and return the user's history.
         """
         dao = HistoriqueDAO()
-        return dao.afficher_historique_utilisateur(adresse_mail)
+        return dao.afficher_historique_utilisateur(self, adresse_mail)
 
     def importer_historique(self, adresse_mail):
         """
         Call the DAO function to import history data.
         """
         dao = HistoriqueDAO()
-        return dao.importer_historique(adresse_mail)
+        return dao.importer_historique(self, adresse_mail)
 
     def exporter_historique(self, adresse_mail):
         """
         Call the DAO function to export history data.
         """
         dao = HistoriqueDAO()
-        return dao.exporter_historique(adresse_mail)
+        return dao.exporter_historique(self, adresse_mail)
 
     def supprimer_historique_utilisateur(self, adresse_mail):
         """
         Call the DAO function to delete user's history.
         """
         dao = HistoriqueDAO()
-        return dao.supprimer_historique_utilisateur(adresse_mail)
+        return dao.supprimer_historique_utilisateur(self, adresse_mail)
 
     def ajouter_historique(self, adresse_mail, URL_page):
         """
         Call the DAO function to add history data.
         """
         dao = HistoriqueDAO()
-        return dao.ajouter_historique(adresse_mail, URL_page)
+        return dao.ajouter_historique(self, adresse_mail, URL_page)
